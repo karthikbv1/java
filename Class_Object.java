6fa6a13dbfb335e79e8927dfb1e7bc14d7ee4598
Karthik Vasanth <karthikbvkarthik87@gmail.com>
2025-03-19 14:17:11 +0530

    Update Class_Object.java

--- Class_Object.java
+++ Class_Object.java
@@ -1,7 +1,7 @@
 package Oops;
 
 public class Class_Object {
-    String name = "Akbar Husain";
+    String name = "firefox";
     public static void main(String[] args) {
         Class_Object obj = new Class_Object();
         System.out.println(obj.name);
